08f42f9730f64630a0d6d146921f5135bf981599
AlisDimit <afalisa013@gmail.com>
2021-09-13 17:41:18 +0300

    Правки по заданию 2

--- src/com/max/idea/Task3.java
+++ src/com/max/idea/Task3.java
@@ -3,10 +3,10 @@ package com.max.idea;
 public class Task3 {
     public static void main(String[] args) {
         int[] arr = {22, 3, 5, 8, 9};
-        int temp = arr[4];
-        arr[4] = arr[0];
+        int temp = arr[arr.length - 1];
+        arr[arr.length - 1] = arr[0];
         arr[0] = temp;
 
-        System.out.println(arr[arr.length - 5] + arr[arr.length - 3]);
+        System.out.println(arr[0] + arr[arr.length / 2]);
     }
 }
